7187927eb04712a3905b4266bd363fdb0b9824fa
Максим Самойлов <samoilowmaxim@gmail.com>
2016-01-02 12:31:14 +0600

    fix: add babel-register

--- karma.conf.js
+++ karma.conf.js
@@ -1,19 +1,16 @@
-/* eslint-disable */
-
 require('babel-register');
 require('webpack');
 require('karma-webpack');
-var path = require('path');
-var webpackConfig = require('./utils/make-webpack-config')();
+const webpackConfig = require('./utils/make-webpack-config').default();
 webpackConfig.devtool = 'inline-source-map';
 
 
-module.exports = function(config) {
-	var browsers = ['Chrome'];
+module.exports = config => {
+	const browsers = ['Chrome'];
 
 	config.set({
 		autoWatch: true,
-		browsers: browsers,
+		browsers,
 		singleRun: false,
 		frameworks: ['mocha', 'chai-things', 'chai'],
 		preprocessors: {
